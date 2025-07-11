a51db6f83705b56429b1599ae3d058813684fd9b
Jason Yang <167345977+jasonshyang@users.noreply.github.com>
2025-07-11 23:09:28 +0100

    bybit client impl

---
+++ crates/clients/src/bybit/types.rs
@@ -0,0 +1,69 @@
+use serde::Deserialize;
+
+pub const BYBIT_ENDPOINT: &str = "wss://stream.bybit.com/v5/public/spot";
+pub type BybitOrderEntry = Vec<String>; // [price, size]
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+pub enum BybitMessage {
+    SubscriptionAck {
+        success: bool,
+        #[serde(rename = "ret_msg")]
+        message: String,
+        #[serde(rename = "conn_id")]
+        connection_id: String,
+        #[serde(rename = "req_id")]
+        request_id: Option<String>,
+        #[serde(rename = "op")]
+        operation: String,
+    },
+    OrderBookUpdate(BybitOrderBookUpdate),
+}
+
+#[derive(Deserialize, Debug)]
+pub struct BybitOrderBookUpdate {
+    /// Topic name
+    pub topic: String,
+    /// The timestamp (ms) that the system generates the data
+    #[serde(rename = "ts")]
+    pub timestamp: u64,
+    /// Data type: snapshot,delta
+    #[serde(rename = "type")]
+    pub data_type: BybitOrderBookDataType,
+    /// Order book data
+    #[serde(rename = "data")]
+    pub data: BybitOrderBookData,
+    /// The timestamp from the matching engine when this orderbook data is produced.
+    /// It can be correlated with T from public trade channel
+    #[serde(rename = "cts")]
+    pub correlated_timestamp: u64,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum BybitOrderBookDataType {
+    Snapshot,
+    Delta,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct BybitOrderBookData {
+    /// Symbol name, e.g. SOLUSDT_SOL/USDT
+    #[serde(rename = "s")]
+    pub symbol: String,
+    /// Bids. For snapshot stream. Sorted by price in descending order
+    /// b[0] is the price, b[1] is the size
+    /// The delta data has size=0, which means that all quotations for this price have been filled or cancelled
+    #[serde(rename = "b")]
+    pub bids: Vec<BybitOrderEntry>,
+    /// Asks. For snapshot stream. Sorted by price in ascending order
+    #[serde(rename = "a")]
+    pub asks: Vec<BybitOrderEntry>,
+    /// Update ID, if receive "u"=1, that is a snapshot data due to the restart of the service.
+    /// Local orderbook should be reset
+    #[serde(rename = "u")]
+    pub update_id: u64,
+    /// Cross sequence
+    #[serde(rename = "seq")]
+    pub sequence: u64,
+}
